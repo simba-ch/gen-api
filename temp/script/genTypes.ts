c4d22e514d4607eafd682649aaf66dbba8315f2d
Simba <18721784110@163.com>
2026-01-20 00:39:32 +0800

    修改引用路径

--- temp/script/genTypes.ts
+++ temp/script/genTypes.ts
@@ -1,20 +1,9 @@
-import { printNode } from "@/utils/printNode.ts";
-import { generateTypes } from "@core/genTypes/index.ts";
-import { componentsInterface, parseOpenapi } from "@utils/parseOpenapi.ts";
+  import { generateTypes } from "@core/genTypes/index.ts";
+import { componentsInterface, parseOpenapi } from "@core/parseOpenapi/index.ts";
 import { pathToFileURL } from "node:url";
 import ts from "typescript";
 const fileName = pathToFileURL("./temp/origin/superHR.openapi.json").href;
 
 await parseOpenapi(fileName);
 
-
-// ast.forEach((node, i) => {
-//     printNode(node, `./temp/types/componentsInterface_${i + 1}.ts`)
-// })
-// // for (let i = 0; i < ast.length; i++) {
-// //     await printNode(ast[i], `./temp/types/componentsInterface_${i + 1}.ts`)
-
-// // }
-
 componentsInterface && generateTypes(componentsInterface as ts.InterfaceDeclaration, './temp/types/superHR-types.ts');
-// componentsInterface && printNode(componentsInterface, `./temp/types/componentsInterface_1.ts`)
