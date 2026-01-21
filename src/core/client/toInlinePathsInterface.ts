14311fdf722425ad2cd4ab4f530a20cb2047eb4d
simba <18721784110@163.com>
2026-01-21 18:57:00 +0800

    添加生成client脚步

--- src/core/client/toInlinePathsInterface.ts
+++ src/core/client/toInlinePathsInterface.ts
@@ -26,6 +26,7 @@ function collectOperationsAST(
   }
 
   return map;
+  
 }
 
 
---
+++ temp/script/genClient.ts
@@ -0,0 +1,7 @@
+import { parseOpenapi } from "@core/parseOpenapi/index.ts";
+import { pathToFileURL } from "node:url";
+
+// 解析 OpenAPI 文件并生成 TypeScript 类型定义文件
+const fileName = pathToFileURL("./temp/origin/todolist.openapi.json").href;
+
+await parseOpenapi(fileName);
